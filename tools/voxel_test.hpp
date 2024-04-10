722d98e3804daa6b240b76b6d435f43cb1d9c7a1
Chestnut45 <cpt20xx@gmail.com>
2024-04-10 15:24:49 -0300

    move voxel test to tools folder, remove src

---
+++ tools/voxel_test.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <chrono>
+#include <string>
+
+#include <imgui/imgui.h>
+
+// Phi engine
+#include <phi/phi.hpp>
+
+class VoxelTest : public Phi::App
+{
+    // Interface
+    public:
+
+        VoxelTest();
+        ~VoxelTest();
+
+        // Update the app, called every frame
+        void Update(float delta) override;
+        
+        // Rendering logic, called every frame
+        void Render() override;
+
+    // Data / implementation
+    private:
+
+        // Main scene
+        Phi::Scene scene;
+
+        // Random number generator
+        Phi::RNG rng{4545};
+
+        // DEBUG: Transform for test voxel mesh
+        Phi::Transform* voxelMeshTransform = nullptr;
+
+        // Settings
+        bool vsync = false;
+        bool fullscreen = false;
+};
