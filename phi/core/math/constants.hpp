2abc689041fb66e85d8e19ef4687b4a4d8ec09cc
Chestnut45 <cpt20xx@gmail.com>
2024-05-03 16:15:52 -0300

    move math constants to separate file

---
+++ phi/core/math/constants.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Phi
+{
+    // Mathematical constants
+    static constexpr float PHI = 1.61803398875f;
+    static constexpr float PI = 3.14159265358f;
+    static constexpr float TAU = 6.28318530718f;
+}
