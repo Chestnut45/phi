52f61230bcddc711c6580c51d62801ec3d703aae
Chestnut45 <cpt20xx@gmail.com>
2024-05-04 21:50:43 -0300

    reformat dithering shader code

--- phi/graphics/shaders/light_scatter.fs
+++ phi/graphics/shaders/light_scatter.fs
@@ -24,22 +24,22 @@ uniform vec4 exposureDecayDensityWeight;
 in vec2 texCoords;
 out vec3 finalColor;
 
-// Analytic bayer over 2 domains
-float bayer2  (vec2 a){a=floor(a);return fract(dot(a,vec2(.5, a.y*.75)));}
-float bayer4  (vec2 a){return bayer2 (  .5*a)*.25    +bayer2(a);}
-float bayer8  (vec2 a){return bayer4 (  .5*a)*.25    +bayer2(a);}
-float bayer16 (vec2 a){return bayer4 ( .25*a)*.0625  +bayer4(a);}
-float bayer32 (vec2 a){return bayer8 ( .25*a)*.0625  +bayer4(a);}
-float bayer64 (vec2 a){return bayer8 (.125*a)*.015625+bayer8(a);}
-float bayer128(vec2 a){return bayer16(.125*a)*.015625+bayer8(a);}
-#define dither2(p)   (bayer2(  p)-.375      )
-#define dither4(p)   (bayer4(  p)-.46875    )
-#define dither8(p)   (bayer8(  p)-.4921875  )
-#define dither16(p)  (bayer16( p)-.498046875)
-#define dither32(p)  (bayer32( p)-.499511719)
-#define dither64(p)  (bayer64( p)-.49987793 )
-#define dither128(p) (bayer128(p)-.499969482)
-//https://www.shadertoy.com/view/4ssfWM
+// Helper bayer matrix dithering functions
+// https://www.shadertoy.com/view/4ssfWM
+float bayer2 (vec2 a) { a = floor(a); return fract(dot(a, vec2(.5, a.y * 0.75))); }
+float bayer4 (vec2 a) { return bayer2(0.5 * a) * 0.25 + bayer2(a); }
+float bayer8 (vec2 a) { return bayer4(0.5 * a) * 0.25 + bayer2(a); }
+float bayer16 (vec2 a) { return bayer4(0.25 * a) * 0.0625 + bayer4(a); }
+float bayer32 (vec2 a) { return bayer8(0.25 * a) * 0.0625 + bayer4(a); }
+float bayer64 (vec2 a) { return bayer8(0.125 * a) * 0.015625 + bayer8(a); }
+float bayer128 (vec2 a) { return bayer16(0.125 * a) * 0.015625 + bayer8(a); }
+#define dither2(p) (bayer2(p) - 0.375)
+#define dither4(p) (bayer4(p) - 0.46875)
+#define dither8(p) (bayer8(p) - 0.4921875)
+#define dither16(p) (bayer16(p) - 0.498046875)
+#define dither32(p) (bayer32(p) - 0.499511719)
+#define dither64(p) (bayer64(p) - 0.49987793)
+#define dither128(p) (bayer128(p) - 0.499969482)
 
 void main()
 {
