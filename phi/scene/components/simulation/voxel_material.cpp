f7d604e6fb811222e8db65ac191a7d4f5337a491
Chestnut45 <cpt20xx@gmail.com>
2024-05-28 23:33:35 -0300

    start voxel material

---
+++ phi/scene/components/simulation/voxel_material.cpp
@@ -0,0 +1,13 @@
+#include <voxel_material.hpp>
+
+namespace Phi
+{
+    VoxelMaterial::VoxelMaterial(const std::string& name, SimulationFlags::type simulationFlags, int pbrID)
+        : name(name), simulationFlags(simulationFlags), pbrID(pbrID)
+    {
+    }
+
+    VoxelMaterial::~VoxelMaterial()
+    {
+    }
+}
---
+++ phi/scene/components/simulation/voxel_material.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace Phi
+{
+    // A material used for simulation with voxel objects
+    struct VoxelMaterial
+    {
+        // Flags used to determine what types of simulation apply to the material
+        struct SimulationFlags
+        {
+            typedef uint32_t type;
+            type value;
+            enum
+            {
+                NONE = 0,
+                LIQUID = 1,
+                POWDER = 1 << 1,
+                FLAMMABLE = 1 << 2,
+                // ...
+            };
+        };
+
+        VoxelMaterial(const std::string& name = "new_material", SimulationFlags::type simulationFlags = SimulationFlags::NONE, int pbrID = 0);
+        ~VoxelMaterial();
+
+        // Identifiers
+        std::string name;
+
+        // Simulation data
+        SimulationFlags::type simulationFlags;
+
+        // PBR material ID for rendering
+        int pbrID;
+    };
+}
